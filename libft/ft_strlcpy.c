4d977de017b6d3cc0dc477394d37bd93398987f0
Tiago Simao Godinho <tsimao-g@c1r2s6.42lisboa.com>
2025-10-20 20:58:36 +0100

    42 - 20/10/2025

---
+++ libft/ft_strlcpy.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsimao-g <tsimao-g@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/20 13:47:29 by tsimao-g          #+#    #+#             */
+/*   Updated: 2025/10/20 14:12:03 by tsimao-g         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+
+size_t ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	int l;
+	size_t i;
+	
+	i = 0;
+	l = 0;
+	while (src[l++]);
+	while (src[i] && i < size - 1)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (i);
+}
